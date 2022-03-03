68a3b4533374f7feca6b3714e30ed03514a95877
Rafa1891 <rufus199117@gmail.com>
2022-03-03 13:42:23 +0100

    nuevas implementaciones swagger,oauth etc, entity rol y usuario

--- apirest-clientes/src/main/java/com/example/demo/apirest/ApirestClientesApplication.java
+++ apirest-clientes/src/main/java/com/example/demo/apirest/ApirestClientesApplication.java
@@ -3,7 +3,13 @@ package com.example.demo.apirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title ="API Clientes",version ="1.0",description ="Crud completo de Clientes"))
 public class ApirestClientesApplication {
 
 	public static void main(String[] args) {
